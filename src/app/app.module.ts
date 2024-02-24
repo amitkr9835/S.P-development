d1528f19b7f8ba937c2d058c0b214950f08dd48f
amitkumar <amitkr9835@gmail.com>
2024-02-24 10:36:16 +0530

    navbar toggle issue and favicon change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { HomeComponent } from './modules/home/home.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HeaderComponent } from './modules/header/header.component';
 import { FooterComponent } from './modules/footer/footer.component';
+import { RegistrationformComponent} from './modules/registrationform/registrationform.component';
 @NgModule ({
     declarations: [
         AppComponent,
         HomeComponent,
         HeaderComponent,
         FooterComponent,
-        AboutComponent
+        AboutComponent,
+        RegistrationformComponent
     ],
     imports: [
         BrowserModule,
